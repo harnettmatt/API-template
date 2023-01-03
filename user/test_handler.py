9df323db959770107581fc7d338cef42f183a23e
Matt Harnett <120770687+harnettmatt@users.noreply.github.com>
2023-01-03 08:34:57 -0500

    feat: stubs for User class (#8)

---
+++ user/test_handler.py
@@ -0,0 +1,25 @@
+"""Unit tests for /users routing handler"""
+from fastapi.testclient import TestClient
+
+from user.handler import ROUTER
+
+
+class TestHandler:
+    """Unit tests for /users routing handler"""
+
+    @staticmethod
+    def test_get_all():
+        """
+        GIVEN: a GET request to /users
+        THEN: a list of Users is returned
+        """
+        with TestClient(ROUTER) as client:
+            response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "identifier": "fake-identifier",
+                "first_name": "John",
+                "last_name": "Smith",
+            }
+        ]
