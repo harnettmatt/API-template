e1258fbd36fd616695e2eaecb7910da6eba150c6
Matt Harnett <120770687+harnettmatt@users.noreply.github.com>
2023-01-02 20:10:30 -0500

    feat: adding loose stubs for rating (#3)
    * feat: adding loose stubs for rating
    
    * chore: adding comment for httpx
    
    * chore: cleaning up comments
    
    * chore: removing unrelated ranking files

---
+++ rating/test_handler.py
@@ -0,0 +1,27 @@
+"""Unit tests for /ratings routing handler"""
+from fastapi.testclient import TestClient
+
+from rating.handler import ROUTER
+
+
+class TestHandler:
+    """Unit tests for /ratings routing handler"""
+
+    @staticmethod
+    def test_get_all():
+        """
+        GIVEN: a GET request to /ratings
+        THEN: a list of ratings is returned
+        """
+        with TestClient(ROUTER) as client:
+            response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "author": "fake-author",
+                "identifier": "fake-identifier",
+                "notes": None,
+                "rating": 1.0,
+                "recipient": "fake-recipient",
+            }
+        ]
