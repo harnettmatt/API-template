7544b416cf860d58c7cf679ccb502f919d324d2b
Matt Harnett <harnettmatt@gmail.com>
2023-01-16 20:44:17 -0500

    feat: adding update functinality to db service

--- database/database_service.py
+++ database/database_service.py
@@ -1,11 +1,11 @@
 """Module containing all database setup"""
-from typing import Type
+from typing import List, Optional, Type
 from uuid import UUID
 
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from persistable.models import Persistable
+from uuid_base_model.schemas import UUIDBaseModel
 
 
 class DatabaseService:
@@ -13,36 +13,50 @@ class DatabaseService:
     Service that interacts with the db layer
     """
 
-    database: Session
+    session: Session
 
-    def __init__(self, database: Session):
-        self.database = database
+    def __init__(self, session: Session):
+        self.session = session
 
-    def get(self, identifier: UUID, model_type: Type[Persistable]):
+    def get(
+        self, identifier: UUID, model_type: Type[Persistable]
+    ) -> Optional[Persistable]:
         """
-        Gets object from db for a given model and identifier
+        Gets instance from db for a given model and identifier
         """
-        return self.database.query(model_type).filter_by(identifier=identifier).first()
+        return self.session.query(model_type).filter_by(identifier=identifier).first()
 
-    def all(self, model_type: Type[Persistable], skip: int = 0, limit: int = 100):
+    def all(
+        self, model_type: Type[Persistable], skip: int = 0, limit: int = 100
+    ) -> List[Persistable]:
         """
-        Gets all objects from db for a given model and optional limiting
+        Gets all instances from db for a given model and optional limiting
         """
-        return self.database.query(model_type).offset(skip).limit(limit).all()
+        return self.session.query(model_type).offset(skip).limit(limit).all()
 
-    def create(self, input_schema: BaseModel, model_type: Type[Persistable]):
+    def create(self, input_schema: UUIDBaseModel, model_type: Type[Persistable]):
         """
-        Creates object in db for a given pydantic input schema and model
+        Creates instance in db for a given pydantic input schema and model
         """
         model_instance = model_type(**input_schema.dict())
-        self.database.add(model_instance)
-        self.database.commit()
-        self.database.refresh(model_instance)
+        self.session.add(model_instance)
+        self.session.commit()
+        self.session.refresh(model_instance)
         return model_instance
 
     def delete(self, identifier: UUID, model_type: Type[Persistable]):
         """
-        Deletes object from db for a given model and identifier
+        Deletes instance from db for a given model and identifier
         """
-        self.database.query(model_type).filter_by(identifier=identifier).delete()
-        self.database.commit()
+        self.session.query(model_type).filter_by(identifier=identifier).delete()
+        self.session.commit()
+
+    def update(self, input_schema: UUIDBaseModel, model_type: Type[Persistable]):
+        """
+        Gets instance from db, merges input_schema with db instance, update db instance
+        """
+        update_dict = input_schema.dict(exclude_none=True)
+        self.session.query(model_type).filter_by(
+            identifier=input_schema.identifier
+        ).update(update_dict)
+        self.session.commit()
