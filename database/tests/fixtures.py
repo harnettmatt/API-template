9afdde29f404f25ce14dd257ece88bd62da59aad
Matt Harnett <120770687+harnettmatt@users.noreply.github.com>
2023-01-16 20:46:18 -0500

    feat: adding update functinality to db service (#29)

--- database/tests/fixtures.py
+++ database/tests/fixtures.py
@@ -1,11 +1,17 @@
+from typing import Optional
+
 from sqlalchemy import Column, String
 
 from persistable.models import Persistable
 from uuid_base_model.schemas import UUIDBaseModel
 
 
-class MockAPIInput(UUIDBaseModel):
+class MockAPICreateInput(UUIDBaseModel):
     foo: str
+
+
+class MockAPIUpdateInput(UUIDBaseModel):
+    foo: Optional[str]
 
 
 class MockPersistable(Persistable):
--- database/tests/test_database_service.py
+++ database/tests/test_database_service.py
@@ -4,12 +4,16 @@ import pytest
 
 from database.database import get_db
 from database.database_service import DatabaseService
-from database.tests.fixtures import MockAPIInput, MockPersistable
+from database.tests.fixtures import (
+    MockAPICreateInput,
+    MockAPIUpdateInput,
+    MockPersistable,
+)
 
 
-@pytest.fixture(name="mock_api_input")
-def fixture_mock_api_input():
-    return MockAPIInput(foo="bar")
+@pytest.fixture(name="mock_api_create_input")
+def fixture_mock_api_create_input():
+    return MockAPICreateInput(foo="bar")
 
 
 @pytest.fixture(name="mock_db_service")
@@ -20,19 +24,21 @@ def fixture_mock_db_service():
 class TestDatabase:
     @staticmethod
     @pytest.mark.integtest
-    def test_create_get(mock_db_service: DatabaseService, mock_api_input: MockAPIInput):
+    def test_flow(
+        mock_db_service: DatabaseService, mock_api_create_input: MockAPICreateInput
+    ):
         # TODO: requires truncation of table before each run
-        expected_persistable = MockPersistable(**mock_api_input.dict())
+        expected_persistable = MockPersistable(**mock_api_create_input.dict())
 
         # CREATE
         response = mock_db_service.create(
-            input_schema=mock_api_input, model_type=MockPersistable
+            input_schema=mock_api_create_input, model_type=MockPersistable
         )
         assert response == expected_persistable
 
         # GET
         response = mock_db_service.get(
-            identifier=mock_api_input.identifier, model_type=MockPersistable
+            identifier=mock_api_create_input.identifier, model_type=MockPersistable
         )
         assert response == expected_persistable
 
@@ -40,11 +46,19 @@ class TestDatabase:
         response = mock_db_service.all(model_type=MockPersistable)
         assert response == [expected_persistable]
 
+        # UPDATE
+        mock_api_update_input = MockAPIUpdateInput(**mock_api_create_input.dict())
+        mock_api_update_input.foo = "new_value"
+        expected_updated_persistable = MockPersistable(**mock_api_update_input.dict())
+        response = mock_db_service.update(
+            input_schema=mock_api_update_input, model_type=MockPersistable
+        )
+        response = mock_db_service.get(
+            identifier=mock_api_update_input.identifier, model_type=MockPersistable
+        )
+        assert response == expected_updated_persistable
+
         # DELETE
         response = mock_db_service.delete(
-            identifier=mock_api_input.identifier, model_type=MockPersistable
+            identifier=mock_api_create_input.identifier, model_type=MockPersistable
         )
-
-        # ALL
-        response = mock_db_service.all(model_type=MockPersistable)
-        assert response == []
